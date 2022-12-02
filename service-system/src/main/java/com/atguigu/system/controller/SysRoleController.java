fbbead403de38b5737319df99df675390f8ca9bc
CodeLeader <1144722582@qq.com>
2022-12-02 19:33:35 +0800

    用户管理-用户分配角色

--- service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
@@ -2,6 +2,7 @@ package com.atguigu.system.controller;
 
 import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
+import com.atguigu.model.vo.AssginRoleVo;
 import com.atguigu.model.vo.SysRoleQueryVo;
 import com.atguigu.system.exception.GuiguException;
 import com.atguigu.system.service.SysRoleService;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
 @Api(tags = "角色管理接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
@@ -96,6 +99,18 @@ public class SysRoleController {
     @DeleteMapping("batchRemove")
     public Result batchRemove(@RequestBody List<Long> ids){
         sysRoleService.removeByIds(ids);
+        return Result.ok();
+    }
+    @ApiOperation("获取用户的角色数据")
+    @GetMapping("toAssign/{userId}")
+    public Result toAssign(@PathVariable String userId){
+        Map<String,Object> roleMap=sysRoleService.getRolesByUserId(userId);
+        return Result.ok(roleMap);
+    }
+    @ApiOperation("用户分配角色")
+    @PostMapping("doAssign")
+    public Result doAssign(@RequestBody AssginRoleVo assginRoleVo){
+        sysRoleService.doAssign(assginRoleVo);
         return Result.ok();
     }
 }
