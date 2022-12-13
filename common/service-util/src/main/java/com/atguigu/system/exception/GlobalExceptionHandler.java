1a681ee268d1456d685055da8a17b979786daf11
CodeLeader <1144722582@qq.com>
2022-12-13 18:25:30 +0800

    SpringSecurity异常处理

--- common/service-util/src/main/java/com/atguigu/system/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/atguigu/system/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.atguigu.system.exception;
 
 import com.atguigu.common.result.Result;
+import com.atguigu.common.result.ResultCodeEnum;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,6 +32,13 @@ public class GlobalExceptionHandler {
     public Result error(GuiguException e){
         e.printStackTrace();
         return Result.fail().message("执行了自定义异常处理");
+    }
+
+    //Spring Security异常
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public Result error(AccessDeniedException e) throws AccessDeniedException {
+        return Result.fail().code(ResultCodeEnum.PERMISSION.getCode()).message("没有当前功能的操作权限");
     }
 
 
