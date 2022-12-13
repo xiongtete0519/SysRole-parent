c87a2036dd0aee4c07a4c6b350795e4add0fdaf5
CodeLeader <1144722582@qq.com>
2022-12-13 18:21:16 +0800

    SpringSecurity用户认证+授权

--- service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
@@ -11,6 +11,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,6 +40,7 @@ public class SysRoleController {
     }
 
     //逻辑删除接口
+    @PreAuthorize("hasAuthority('bnt.sysRole.remove')")
     @ApiOperation("逻辑删除接口")
     @DeleteMapping("remove/{id}")
     public Result remove(@PathVariable String id){
@@ -49,6 +51,8 @@ public class SysRoleController {
             return Result.fail();
         }
     }
+
+    @PreAuthorize("hasAuthority('bnt.sysRole.list')")
     //条件分页查询
     @ApiOperation("条件分页查询")
     @GetMapping("{page}/{limit}")
@@ -66,6 +70,7 @@ public class SysRoleController {
     //@RequestBody 不能使用get提交方式
     //传递json格式数据，把json格式数据封装到对象里面
     @ApiOperation("添加角色")
+    @PreAuthorize("hasAuthority('bnt.sysRole.add')")
     @PostMapping("save")
     public Result saveRole(@RequestBody SysRole sysRole){
         boolean isSuccess = sysRoleService.save(sysRole);
@@ -76,13 +81,15 @@ public class SysRoleController {
         }
     }
     //根据id查询
-    @ApiOperation("根据id查询")
+    @PreAuthorize("hasAuthority('bnt.sysRole.list')")
     @PostMapping("findRoleById/{id}")
+    @ApiOperation("根据id查询")
     public Result findRoleById(@PathVariable Long id){
         SysRole sysRole = sysRoleService.getById(id);
         return Result.ok(sysRole);
     }
     //最终修改
+    @PreAuthorize("hasAuthority('bnt.sysRole.update')")
     @ApiOperation("最终修改")
     @PostMapping("update")
     public Result updateRole(@RequestBody SysRole sysRole){
@@ -95,6 +102,7 @@ public class SysRoleController {
     }
     //批量删除[1,2,3]
     //json数组格式对应Java的list集合
+    @PreAuthorize("hasAuthority('bnt.sysRole.remove')")
     @ApiOperation("批量删除")
     @DeleteMapping("batchRemove")
     public Result batchRemove(@RequestBody List<Long> ids){
